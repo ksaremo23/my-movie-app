3eefcdf92d9620e88aad18d8e467b0c86cb31d85
Leicsha Karen Saremo <120180371+ksaremo23@users.noreply.github.com>
2025-07-21 13:28:20 +0800

    6th

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: '/my-movie-app/',
+//   // "server.hmr.overlay" : false,
+//   server: {
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/my-movie-app/',
-  // "server.hmr.overlay" : false,
+  base: '/my-movie-app/', // MUST match your repo name
   server: {
     hmr: {
       overlay: false,
